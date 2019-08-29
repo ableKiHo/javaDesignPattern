09aa2806bec9e63bcb4ce0187a0a6db5e19f80ac
ukh <able.kiho.um@gmail.com>
2019-08-30 00:00:38 +0900

    6.2 인터페이스를 활용한 다중상

--- src/CH1/OOP/RectangleShape.java
+++ src/CH1/OOP/RectangleShape.java
@@ -1,56 +1,16 @@
 package CH1.OOP;
 
-public class RectangleShape implements IDrawable{
-
-    private int x;
-    private int y;
-
-    private int width;
-    private int height;
+public class RectangleShape extends AbstractBoundingShape implements IShape {
 
     public RectangleShape() {}
-
-    public RectangleShape(int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
 
     @Override
     public void draw() {
         System.out.println("draw Rectangle");
     }
 
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
+    @Override
+    public boolean contains(int x, int y) {
+        return false;
     }
 }
--- src/CH1/OOP/RoundRectangleShape.java
+++ src/CH1/OOP/RoundRectangleShape.java
@@ -1,54 +1,11 @@
 package CH1.OOP;
 
-public class RoundRectangleShape implements IDrawable {
-
-    private int x;
-    private int y;
-
-    private int width;
-    private int height;
+public class RoundRectangleShape extends AbstractBoundingShape implements IShape {
 
     private float arcWidth;
     private float arcHeight;
 
     public RoundRectangleShape() {
-    }
-
-    @Override
-    public void draw() {
-        System.out.println("draw RoundRectangle");
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
     }
 
     public float getArcWidth() {
@@ -65,5 +22,15 @@ public class RoundRectangleShape implements IDrawable {
 
     public void setArcHeight(float arcHeight) {
         this.arcHeight = arcHeight;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("draw RoundRectangle");
+    }
+
+    @Override
+    public boolean contains(int x, int y) {
+        return false;
     }
 }
