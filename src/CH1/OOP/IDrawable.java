b19f3a8aa78c73b1b76df6c4afb97c2df58bfe5a
ukh <able.kiho.um@gmail.com>
2019-08-29 23:35:43 +0900

    5.3 인터페이스 상속

---
+++ src/CH1/OOP/IDrawable.java
@@ -0,0 +1,7 @@
+package CH1.OOP;
+
+public interface IDrawable {
+
+    //추상메서드를 선언한다.
+    public void draw();
+}
---
+++ src/CH1/OOP/IShape.java
@@ -0,0 +1,6 @@
+package CH1.OOP;
+
+public interface IShape extends IDrawable {
+
+    public boolean contains(int x, int y);
+}
