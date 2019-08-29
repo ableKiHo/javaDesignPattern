3b3b82d0230be86df6c4d2965e7bcea0e11bf326
ukh <able.kiho.um@gmail.com>
2019-08-29 23:56:11 +0900

    6.1 단일 인터페이스 구현

--- src/CH1/OOP/RoundRectangleShape.java
+++ src/CH1/OOP/RoundRectangleShape.java
@@ -1,12 +1,54 @@
 package CH1.OOP;
 
-public class RoundRectangleShape extends AbstractBoundingShape {
+public class RoundRectangleShape implements IDrawable {
+
+    private int x;
+    private int y;
+
+    private int width;
+    private int height;
 
     private float arcWidth;
     private float arcHeight;
 
+    public RoundRectangleShape() {
+    }
+
+    @Override
     public void draw() {
         System.out.println("draw RoundRectangle");
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     public float getArcWidth() {
